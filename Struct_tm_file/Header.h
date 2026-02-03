683f1ec455dd89398cad391429713c89ec637c13
`decisixns <decisixnsqq@gmail.com>
2026-02-03 03:42:16 +0300

    Refactor DateArr structure and update function signatures
    Refactor DateArr structure to initialize members in constructor and update function signatures.

--- Struct_tm_file/Header.h
+++ Struct_tm_file/Header.h
@@ -1,25 +1,40 @@
 ﻿#pragma once
-
 #include <iostream>
+#include <ctime>
 
 using namespace std;
 
-
 struct DateArr
 {
-	tm** arr = nullptr;
-	rsize_t size = 0;
+    tm** arr;
+    int size;
+
+    DateArr()
+    {
+        arr = nullptr;
+        size = 0;
+    }
 };
-void InitDate(tm& date,int y, int m, int d);
+
+// базовые функции
+void InitDate(tm& date, int y, int m, int d);
 void PrintDate(const tm& date);
 void PrintDate(const DateArr& arrDate);
 
+// работа с массивом
 void InsertByIndex(DateArr& arrDate, tm* newDate, int index);
 void AddLast(DateArr& arrDate, tm* newDate);
-void DellByIndex(DateArr& arrDate, tm* newDate, int index);
-int FindFirst(DateArr& arrDate, tm* date);
+void DellByIndex(DateArr& arrDate, int index);
+int FindFirst(const DateArr& arrDate, tm* date);
+
+// сортировка
 bool CompareDate(tm* date1, tm* date2);
 void Sort(DateArr& arrDate);
+void InsertSort(DateArr& arrDate);
 
-void SaveToFile(const char* filePath, const DateArr&);
-void ReadFromFile(const char* filePath,  DateArr&);
+// файлы
+void SaveToFile(const char* filePath, const DateArr& arrDate);
+void ReadFromFile(const char* filePath, DateArr& arrDate);
+
+// очистка памяти
+void FreeArr(DateArr& arrDate);
